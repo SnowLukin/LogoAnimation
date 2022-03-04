04c050e79be9d5be7e065807effca5fd6c5edaee
Snow Lukin <everyoursden@gmail.com>
2022-03-04 23:15:31 +0300

    Initial Commit

---
+++ LogoAnimation/LogoAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogoAnimationApp.swift
+//  LogoAnimation
+//
+//  Created by Snow Lukin on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LogoAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
