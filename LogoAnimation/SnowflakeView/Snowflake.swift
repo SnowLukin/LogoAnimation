4eeb8854d431b9f8c8cf945cccaab129284ca2d2
Snow Lukin <everyoursden@gmail.com>
2022-03-06 20:06:55 +0300

    Cosmetic fixes.

--- LogoAnimation/SnowflakeView/Snowflake.swift
+++ LogoAnimation/SnowflakeView/Snowflake.swift
@@ -15,24 +15,24 @@ struct Snowflake: View {
             let height = geometry.size.height
             let size = min(width, height)
             
-            let closeLine = size * 0
-            let farLine = size - size / 3
-            let middleLine = size / 2 - size / 6
+            let startPoint = size * 0
+            let endPoint = size - size / 3
+            let middlePoint = size / 2 - size / 6
             
             ForEach(0..<6) { iteration in
                 SnowflakeLeave()
                     .frame(width: size / 3.1, height: size / 3.1)
                     .rotationEffect(.degrees(Double(iteration) * 60))
                     .offset(
-                        x: iteration > 3 ? closeLine : (1...2).contains(iteration) ? farLine : middleLine,
-                        y: iteration == 0 ? closeLine : iteration == 3 ? farLine : iteration == 1 || iteration == 5 ? middleLine / 2 : middleLine + middleLine / 2
+                        x: iteration > 3 ? startPoint : (1...2).contains(iteration) ? endPoint : middlePoint,
+                        y: iteration == 0 ? startPoint : iteration == 3 ? endPoint : iteration == 1 || iteration == 5 ? middlePoint / 2 : middlePoint + middlePoint / 2
                     )
                     
             }
             SnowflakeSpikes()
             SnowflakeMiddleCircle()
                 .frame(width: size / 4, height: size / 4)
-                .offset(x: middleLine * 1.13, y: middleLine * 1.12)
+                .offset(x: middlePoint * 1.13, y: middlePoint * 1.12)
         }
     }
 }
